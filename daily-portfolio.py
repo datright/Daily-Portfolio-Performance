05d9aeb23cefd02d9d6af2010a7838630ed2211a
datright <84527629+datright@users.noreply.github.com>
2021-06-22 19:44:16 -0400

    Requests from AlphaAdvantage

--- daily-portfolio.py
+++ daily-portfolio.py
@@ -18,3 +18,48 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+symbol = "MSFT"
+
+# 1. INFO INPUTS
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={ALPHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+#print(type(response)) #> requests.models.Response
+#print(response.text) #> 200
+
+parsed_response = json.loads(response.text)
+
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+tsd = parsed_response["Time Series (Daily)"]
+
+dates = list(tsd.keys()) # TODO: sort to ensure latest day is first. currently assuming latest day is on top 
+
+latest_day = dates[0]
+
+latest_close = tsd[latest_day]["4. close"]
+
+#breakpoint()
+
+
+
+# 2. INFO OUTPUTS
+
+print("-------------------------")
+print("SELECTED SYMBOL: XYZ")
+print("-------------------------")
+print("REQUESTING STOCK MARKET DATA...")
+print("REQUEST AT: 2018-02-20 02:00pm")
+print("-------------------------")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
+print("RECENT HIGH: $101,000.00")
+print("RECENT LOW: $99,000.00")
+print("-------------------------")
+print("RECOMMENDATION: BUY!")
+print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print("HAPPY INVESTING!")
+print("-------------------------")
