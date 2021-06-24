9ccc81f2eb2db64b33b3222983a189542e7e08df
datright <84527629+datright@users.noreply.github.com>
2021-06-24 18:38:56 -0400

    Code to fetch data using inputs from csv file

--- daily-portfolio.py
+++ daily-portfolio.py
@@ -33,69 +33,73 @@ ALPHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
 
 import csv
 
-with open('portfolio.csv','w+') as file:
-    myFile=csv.writer(file)
-    myFile.writerow(["stock", "shares"])
-    noOfStocks=int(input("Please enter the number of different stocks you own: "))
-    for i in range (noOfStocks):
-        Stock=input("Company " + str(i +1)+ " : What is the ticker of the stock you own? ")
-        Shares=input("Company " + str(i +1)+ ": How many shares do you own? ")
-        myFile.writerow([Stock,Shares])
+#with open('portfolio.csv','w+') as file:
+#    myFile=csv.writer(file)
+#    myFile.writerow(["stock", "shares"])
+#    noOfStocks=int(input("Please enter the number of different stocks you own: "))
+#    for i in range (noOfStocks):
+#        Stock=input("Company " + str(i +1)+ " : What is the ticker of the stock you own? ")
+#        Shares=input("Company " + str(i +1)+ ": How many shares do you own? ")
+#        myFile.writerow([Stock,Shares])
 
 
 # 1. INFO INPUTS
+from pandas import read_csv
+df = read_csv('portfolio.csv')
+#Stock= df["Stock"]
+print(df.head())
+portfolio=df.to_dict("records")
+for row in portfolio:
+    print(row)
 
-request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={Stock}&apikey={ALPHAVANTAGE_API_KEY}"
-
-response = requests.get(request_url)
-#print(type(response)) #> requests.models.Response
-#print(response.text) #> 200
-
-parsed_response = json.loads(response.text)
-
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-
-tsd = parsed_response["Time Series (Daily)"]
-
-dates = list(tsd.keys()) # TODO: sort to ensure latest day is first. currently assuming latest day is on top 
-
-latest_day = dates[0]
-prior_day = dates[1]
-latest_close = tsd[latest_day]["4. close"]
-latest_open = tsd[latest_day]["1. open"]
-prior_close = tsd[prior_day]["4. close"]
+    print(row["Stock"])
 
 
-print(prior_close)
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={row['Stock']}&apikey={ALPHAVANTAGE_API_KEY}"
 
-import pandas
-df = pandas.read_csv('portfolio.csv')
-print(df)
+    response = requests.get(request_url)
+
+    parsed_response = json.loads(response.text)
+
+    tsd = parsed_response["Time Series (Daily)"]
+
+    dates = list(tsd.keys()) # TODO: sort to ensure latest day is first. currently assuming latest day is on top 
+
+    latest_day = dates[0]
+
+    latest_close = to_usd(float(tsd[latest_day]["4. close"]))
+    latest_open = to_usd(float(tsd[latest_day]["1. open"]))
+    print(f"LATEST CLOSE: {latest_close}")
+    print(f"LATEST OPEN: {latest_open}")
+
+
+#from pandas import read_csv
+#df = read_csv("test.csv")
+#print(df.head())
+
+
+#col_list = ["Stock", "Shares"]
+#df = pandas.read_csv('portfolio.csv',usecols=col_list)
+#print(df)
 
 #breakpoint()
 
 
 
-# 2. INFO OUTPUTS)
-int_latest = float(latest_close)
-int_prior = float(prior_close)
-
-daily_px = int_latest/int_prior-1
-percentage = "{:.00%}".format(daily_px)
-daily_pd = int_latest-int_prior
+# 2. INFO OUTPUTS
 
 
-print(f"SELECTED SYMBOL: {stock}")
-print("-------------------------")
-print(f"SELECTED SYMBOL: {symbol}")
-print("REQUEST AT: ", current_time.strftime("%Y-%m-%d %I:%M %p"))
-print(f"LATEST DAY: {last_refreshed}")
-print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
-print(f"LATEST OPEN: {to_usd(float(latest_open))}")
+#print(f"SELECTED SYMBOL: {Stock}")
+#print("-------------------------")
+#print(f"SELECTED SYMBOL: {Stock}")
 
-print(f"PRIOR DAY CLOSE: {to_usd(float(prior_close))}")
-print(f"DAILY $ CHANGE: ", to_usd(daily_pd))
-print(f"DAILY % CHANGE: ", percentage)
+#print(latest_close)
+#print(latest_open)
+
+print(f"LATEST CLOSE: {latest_close}")
+print(f"LATEST OPEN: {latest_open}")
+
+print("test")
 
 
 SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY")
